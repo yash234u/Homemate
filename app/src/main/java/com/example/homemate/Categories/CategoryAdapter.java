a8a880ef9ca73320285d93b7bd63e09af77a2634
tanvi.sawant68@gmail.com <tanvi.sawant68@gmail.com>
2024-03-29 14:53:24 +0530

    Home page slider

---
+++ app/src/main/java/com/example/homemate/Categories/CategoryAdapter.java
@@ -0,0 +1,58 @@
+package com.example.homemate.Categories;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.homemate.R;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.viewHolder> {
+
+    private Context context;
+    private List<CategoryModel> list;
+
+    public CategoryAdapter(Context context, List<CategoryModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_list ,parent,false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+        Glide.with(context).load(list.get(position).getImg_url()).into(holder.catImg);
+        holder.catName.setText(list.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+
+        ImageView catImg;
+        TextView catName;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            catImg = itemView.findViewById(R.id.cat_img);
+            catName = itemView.findViewById(R.id.cat_name);
+        }
+    }
+}
--- app/src/main/java/com/example/homemate/HomeFragment.java
+++ app/src/main/java/com/example/homemate/HomeFragment.java
@@ -3,7 +3,10 @@ package com.example.homemate;
 import android.media.Image;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +15,30 @@ import android.view.ViewGroup;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.example.homemate.Categories.CategoryAdapter;
+import com.example.homemate.Categories.CategoryModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class HomeFragment extends Fragment {
+
+    //Popular Categories Recycler View
+    RecyclerView catRecyclerview;
+
+    CategoryAdapter categoryAdapter;
+    List<CategoryModel> categoryModelList;
+
+    //Firebase Connect
+    private DatabaseReference db;
+
 
     public HomeFragment(){
 
@@ -26,7 +47,12 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        catRecyclerview = root.findViewById(R.id.rec_category);
+
+        db = FirebaseDatabase.getInstance().getReference();
+
 
         //image slider
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
@@ -37,6 +63,14 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.banner3, ScaleTypes.CENTER_CROP));
 
         imageSlider.setImageList(slideModels);
+
+
+        //Popular Categories
+        catRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoryModelList = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(getContext(),categoryModelList);
+        catRecyclerview.setAdapter(categoryAdapter);
+
         return root;
     }
 }
